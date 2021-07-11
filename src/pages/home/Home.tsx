4f4d9963e6f9fcc9620bfdfd2fc94797bc9d8292
maxichiale <maxi.chiale@gmail.com>
2021-07-10 23:53:35 -0300

    Added home page.

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+
+
+    return (
+        <div className="home">
+            <h1>The More Population Game</h1>
+            <p>Who has more population?</p>
+            <button> Play</button>
+        </div>
+    );
+}
+export default Home;
