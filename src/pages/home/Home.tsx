34d6bec4be150d45e07066efc970889688c7510f
maxichiale <maxi.chiale@gmail.com>
2021-07-11 02:37:13 -0300

    Home styles.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,15 +3,24 @@ import {
     Link
 } from "react-router-dom";
 import './Home.css';
-
+import background from './../../images/Poblacion_Mundial_746x419.jpeg'
 function Home() {
 
 
     return (
         <div className="home">
-            <h1>The More Population Game</h1>
-            <p>Who has more population?</p>
-            <Link to="/game">Play</Link>
+            <img src={background} className="background" alt="logo" />
+
+            <div className="info">
+                <div className="titleBox">
+
+                <h1 className="title">The More Population Game</h1>
+                </div>
+
+                <p className="subtitle">Who has more population?</p>
+                <Link className="play" to="/game">Play</Link>
+            </div>
+
         </div>
     );
 }
