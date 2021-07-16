c72542241ecbdf7318deaa80f8dd79378b7f7ee6
maxichiale <maxi.chiale@gmail.com>
2021-07-16 00:41:02 -0300

    add style for lost page.

--- src/pages/lost/Lost.tsx
+++ src/pages/lost/Lost.tsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
 import {
     Link
 } from "react-router-dom";
 import './Lost.css';
 
-function Lost(props: any) {
 
+async function getImage() {
+    const response = await fetch('https://some-random-api.ml/meme')
+        .then(response => response.json());
+
+    return response.image;
+}
+function Lost(props: any) {
+    const [image,setImage] = useState();
+    useEffect(() => {
+        getImage();
+        }, []);
+
+    const getImage = async () => {
+        const response = await fetch('https://some-random-api.ml/meme');
+        const data = await response.json();
+        setImage(data.image);
+    };
 
     return (
         <div className="lost">
             <h1>You lost!</h1>
             <p>Your score: {props.score}</p>
-            <Link to="/home">Home</Link>
-            <Link to="/game">Play again</Link>
+            <img className="memeImage" src={image}  alt="meme" />
+            <div className="linksBox">
+            <Link className="link" to="/home">Home</Link>
+            <Link className="link" to="/game">Play again</Link>
+            </div>
         </div>
     );
 }
